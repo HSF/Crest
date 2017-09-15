9851cbec11a466628705c889cf4e99d139f429a1
formica <andrea.formica@cern.ch>
2017-09-15 14:52:30 +0200

    add security aspect.

--- crestdb-web/src/main/java/hep/crest/server/aspects/TagSecurityAspect.java
+++ crestdb-web/src/main/java/hep/crest/server/aspects/TagSecurityAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
@@ -26,9 +27,14 @@ public class TagSecurityAspect {
 	@Before("execution(* hep.crest.server.services.TagService.insertTag(*)) && args(dto)")
 	public void checkRole(TagDto dto) {
 		log.debug("Tag insertion should verify the tag name :"+dto.getName());
-		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		log.debug("Tag insertion should verify the role for user :"+((user == null) ? "none" : user.getUsername()));
-		user.getAuthorities().stream().forEach(s -> log.debug("Role is "+s.getAuthority()));
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth == null) {
+			log.debug("Stop execution....");
+		} else {
+			User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			log.debug("Tag insertion should verify the role for user :"+((user == null) ? "none" : user.getUsername()));
+			user.getAuthorities().stream().forEach(s -> log.debug("Role is "+s.getAuthority()));
+		}
 	}
 	
 
