5c9a561de7179d8fd0ef3a8025878e825eeb880e
formica <andrea.formica@cern.ch>
2018-10-26 17:24:44 +0200

    add missing python.

---
+++ crestdb-client/python/client/crestapi/apis/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .admin_api import AdminApi
+from .fs_api import FsApi
+from .globaltagmaps_api import GlobaltagmapsApi
+from .globaltags_api import GlobaltagsApi
+from .iovs_api import IovsApi
+from .monitoring_api import MonitoringApi
+from .payloads_api import PayloadsApi
+from .runinfo_api import RuninfoApi
+from .tags_api import TagsApi
