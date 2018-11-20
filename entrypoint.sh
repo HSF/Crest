de0cd563ecae81ccc4cbd2e7a76208c304e2cd92
formica <andrea.formica@cern.ch>
2018-11-20 15:20:06 +0100

    add application properties to avoid use of setenv.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
 ## -Dlogging.config=/data/logs/logback.xml
+
+echo "Setting JAVA_OPTS from file javaopts.properties"
+joptfile=./javaopt.properties
+echo "use opt : "
+cat $joptfile
+if [ -e $joptfile ]; then
+   export JAVA_OPTS=
+   while read line; do echo $line; JAVA_OPTS="$JAVA_OPTS -D$line"; done < $joptfile
+fi
+if [ -z "$crest_dir" ]; then
+   crest_dir=$PWD/crestdb-web/build/libs
+fi 
+
+echo "$USER is starting server with JAVA_OPTS : $JAVA_OPTS from user directory $PWD"
 if [ x"$1" = x"" ]; then
     sh -c "java $JAVA_OPTS -jar ${crest_dir}/crest.war"
 else
