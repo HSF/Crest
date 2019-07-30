d6ad34fc2dafcf8822855e7004e4b2a3bede9b90
formica <andrea.formica@cern.ch>
2019-07-30 15:45:20 +0200

    use same as nsw project.

--- vuecompile.sh
+++ vuecompile.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd ./web-ui/crest-ui
-npm install --save axios
-npm install --save buefy
-npm update caniuse-lite browserslist
+npm install
+#npm install --save axios
+#npm install --save buefy
+#npm update caniuse-lite browserslist
 npm run build
