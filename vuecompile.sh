01fb3bc1762011ac65d2755c08c0ae43e607854d
formica <andrea.formica@cern.ch>
2019-07-30 15:54:32 +0200

    update gitlab ci.

--- vuecompile.sh
+++ vuecompile.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd ./web-ui/crest-ui
+
 npm install
 #npm install --save axios
 #npm install --save buefy
-#npm update caniuse-lite browserslist
+npm update caniuse-lite browserslist
 npm run build
