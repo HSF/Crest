07975e076cc074e5f64dafd1dacb5ed212402ae9
Aurianne Vigier <avigier@dedippcx11.extra.cea.fr>
2019-08-22 11:22:42 +0200

    Form for add global tag map

---
+++ web-ui/crest-ui/src/store/modules/db/globaltagmaps.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+export default {
+	namespaced: true,
+	state: {
+		globaltagmap: {
+			/*
+			label: {
+				"globalTagName": "string",
+				"tagName": "string",
+				"record": "string",
+				"label": "string"
+			}
+			*/
+		}
+	},
+	getters: {
+	},
+	mutations: {
+		mergeNewGlobalTagMap(state, globaltagmap) {
+			let label = globaltagmap.label;
+			if (!(label in state.globaltagmap)) {
+				Vue.set(state.globaltagmap, label, globaltagmap)
+			}
+		}
+	},
+	actions: {
+		createGlobalTagMap({commit}, setGlobalTagMap) {
+			const config = {'Content-Type': 'application/json'};
+			const data = JSON.stringify({globalTagName: setGlobalTagMap.globalTagName, tagName: setGlobalTagMap.tagName,
+				record: setGlobalTagMap.record, label: setGlobalTagMap.label});
+			return axios
+			.post(`/crestapi/globaltagmaps`, data, {headers: config})
+			.then(response => response.data)
+			.then(globaltagmap => commit('mergeNewGlobalTagMap', globaltagmap))
+			.catch(error => { return Promise.reject(error) });
+		},
+	}
+}
