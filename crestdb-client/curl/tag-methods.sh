86546d1241e9a557064d62c51e458e8f9e6e601f
formica <andrea.formica@cern.ch>
2019-10-12 14:40:16 +0200

    update curl clients.

--- crestdb-client/curl/tag-methods.sh
+++ crestdb-client/curl/tag-methods.sh
@@ -13,9 +13,10 @@ generate_post_data()
 EOF
 }
 host=$1
+apiname=$2
 tagdata="$(generate_post_data)"
 echo "Use data $tagdata"
-resp=`curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" "${host}/crestapi/tags" --data "${tagdata}"`
+resp=`curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" "${host}/${apiname}/tags" --data "${tagdata}"`
 echo "Received response $resp"
 echo "Try to get back data from server to see if tag is there:"
-curl -X GET "${host}/crestapi/tags?by=name:TAG"
+curl -X GET "${host}/${apiname}/tags?by=name:TAG"
--- crestdb-client/curl/test-multi-upload.sh
+++ crestdb-client/curl/test-multi-upload.sh
@@ -8,22 +8,34 @@ generate_post_data()
   "niovs": 2,
   "format": "PYL",
   "resources":[
-  { "since" : $since1, "payloadHash": "file:///tmp/test-01.txt"},
-  { "since" : $since2, "payloadHash": "file:///tmp/test-02.txt"}
+  { "since" : $since1, "payloadHash": "file:///tmp/newfile-01.txt"},
+  { "since" : $since2, "payloadHash": "file:///tmp/newfile-02.txt"}
   ]
 }
 EOF
 }
+
+function get_rndm_pyld() {
+  dd if=/dev/urandom of=/tmp/newfile.stuff bs=1m count=10
+}
+
 host=$1
-tag=$2
+apiname=$2
+tag=$3
+
+get_rndm_pyld
+
 for a in {1001..1010}; do echo $a;
   b=$((a + 1000))
-  echo "another blob 1 is $a" > /tmp/test-01.txt
-  echo "another blob 2 is $b" > /tmp/test-02.txt
+  echo "another blob 1 is $a" > /tmp/newfile-01.txt
+  echo "another blob 2 is $b" > /tmp/newfile-02.txt
+  cat /tmp/newfile.stuff >> /tmp/newfile-01.txt
+  cat /tmp/newfile.stuff >> /tmp/newfile-02.txt
+
   since1=$a
   since2=$b
   echo $(generate_post_data)
-  resp=`curl --form tag=$tag --form endtime=0 --form iovsetupload="$(generate_post_data)"  --form "files=@/tmp/test-01.txt" --form "files=@/tmp/test-02.txt"  "${host}/crestapi/payloads/uploadbatch"`
+  resp=`curl --form tag=$tag --form endtime=0 --form iovsetupload="$(generate_post_data)"  --form "files=@/tmp/newfile-01.txt" --form "files=@/tmp/newfile-02.txt"  "${host}/${apiname}/payloads/uploadbatch"`
   echo "Received response $resp"
   sleep 1
 done
